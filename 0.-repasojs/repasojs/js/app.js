428b12d8b23f735bbd9ac97a155633b4b3d14a43
Vlad <vlad.jerez@gmail.com>
2021-08-24 10:30:01 -0400

    functions and prototypes

--- 0.-repasojs/repasojs/js/app.js
+++ 0.-repasojs/repasojs/js/app.js
@@ -1,26 +1,30 @@
 'use strict'
 
+// FUNCIONES
+
+function mostrarTarea(tarea, urgencia){
+  return `La tarea ${tarea} tiene una prioridad de ${urgencia}`;
+}
+
+
+
 // OBJETOS
 
 // Object Literal. --------------------------------
-
 const persona = {
     nombre: 'Vladimir',
     profesion: 'Desarrollador Web',
     edad: 37
 }
-console.log(persona);
+// console.log(persona);
 
 // Object constructor.   -------------------------
-
  function Tarea(nombre, urgencia){
 
-    this.nombre = nombre;
-    this.urgencia = urgencia;
+    this.nombre=nombre;
+    this.urgencia=urgencia;
  }
 
- const tarea1 = Tarea('Aprender Js y React', 'Urgente');
- const tarea2 = Tarea('Hacer Ejercicio Diariamente', 'Urgente');
- const tarea3 = Tarea('Practicar el Romanticismo', 'Urgente');
- const tarea4 = Tarea('Pulir Habilidades Laravel', 'Urgente');
-
+const tarea1 = Tarea('Aprender Js y React', 'Urgente');
+const infoTarea = mostrarTarea(tarea1.nombre, tarea1.urgencia);
+console.log(infoTarea);
--- 0.-repasojs/repasojs/js/objects.js
+++ 0.-repasojs/repasojs/js/objects.js
@@ -2,3 +2,25 @@
 
 // OBJETOS
 
+// Object Literal. --------------------------------
+
+const persona = {
+    nombre: 'Vladimir',
+    profesion: 'Desarrollador Web',
+    edad: 37
+}
+console.log(persona);
+
+// Object constructor.   -------------------------
+
+ function Tarea(nombre, urgencia){
+
+    this.nombre = nombre;
+    this.urgencia = urgencia;
+ }
+
+ const tarea1 = Tarea('Aprender Js y React', 'Urgente');
+ const tarea2 = Tarea('Hacer Ejercicio Diariamente', 'Urgente');
+ const tarea3 = Tarea('Practicar el Romanticismo', 'Urgente');
+ const tarea4 = Tarea('Pulir Habilidades Laravel', 'Urgente');
+
